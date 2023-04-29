72a52e8deb37ebf6f15da97c9972c0c2b04f5ad2
Artem Filkovskyi <avfilkovsky@gmail.com>
2023-04-29 14:43:34 +0100

    removed randomly add unnecessary file and fixed link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //You can edit ALL of the code here
 const getData = async () => {
   try {
-    const response = await fetch('http://api.tvmaze.com/shows/82/episodes')
+    const response = await fetch('https://api.tvmaze.com/shows/82/episodes')
     const data = await response.json()
     const dataArr = [...data]
     console.log(data)
