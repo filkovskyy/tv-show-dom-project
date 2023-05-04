6cb16590c280cfd0479f96b811701614a696673a
filkovskyy <avfilkovsky@gmail.com>
2023-05-04 22:54:48 +0100

    completed LVL350

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ async function setup() {
     const response = await fetch('https://api.tvmaze.com/shows/82/episodes')
     const data = await response.json()
     const dataArr = [...data]
-    // console.log(data)
+    console.log(dataArr)
     const globalContainer = document.querySelector('.grid-container')
     const input = document.getElementById('search')
     const numOfEpisodes = document.querySelector('#number_of_episodes')
     input.addEventListener('input', () => search(input))
     numOfEpisodes.innerHTML = `Displaying ${data.length}/${dataArr.length} episodes` // Display the number of episodes
     dataArr.forEach((data) => renderElements(data, globalContainer))
+    setDropDownMenu(dataArr)
+    scrollToElement(dataArr)
   } catch (error) {
     console.log(error)
   }
@@ -64,6 +66,37 @@ function renderElements(element, parentElement) {
   title.append(link)
   dataEl.append(image)
   dataEl.append(description)
+}
+
+function setDropDownMenu(episodes) {
+  const select = document.querySelector('#episode_selector')
+  episodes.forEach((episode) => {
+    let option = document.createElement('option')
+    option.setAttribute(
+      'value',
+      `S${formatSeriesNumber(episode.season)}E${formatSeriesNumber(
+        episode.number
+      )}`
+    )
+    option.innerHTML = `${episode.name} - S${formatSeriesNumber(
+      episode.season
+    )}E${formatSeriesNumber(episode.number)}`
+    select.append(option)
+  })
+}
+
+function scrollToElement(episodes) {
+  const select = document.querySelector('#episode_selector')
+  select.addEventListener('change', () => {
+    const episode = episodes.find(
+      (episode) =>
+        `S${formatSeriesNumber(episode.season)}E${formatSeriesNumber(
+          episode.number
+        )}` === select.value
+    )
+    const selectedEl = document.getElementById(`${episode.id}`)
+    selectedEl.scrollIntoView({ behavior: 'smooth' })
+  })
 }
 
 window.onload = setup
