48e3c1d52563a70f763822e5fa5c069d92ca80f4
filkovskyy <avfilkovsky@gmail.com>
2023-05-04 17:10:00 +0100

    completed LVL 200

--- script.js
+++ script.js
@@ -1,47 +1,69 @@
-//You can edit ALL of the code here
-const getData = async () => {
+async function setup() {
   try {
     const response = await fetch('https://api.tvmaze.com/shows/82/episodes')
     const data = await response.json()
     const dataArr = [...data]
-    console.log(data)
-    const globalContainer = document.querySelector('.global')
-    dataArr.forEach((data) => {
-      const dataEl = document.createElement('div')
-      const title = document.createElement('h2')
-      const link = document.createElement('a')
-      const image = document.createElement('img')
-      const description = document.createElement('p')
-      dataEl.setAttribute('id', data.id)
-      dataEl.setAttribute('class', 'post-container')
-      image.setAttribute('src', data.image.medium)
-      link.setAttribute('href', data.url)
-      link.innerHTML = `${data.name} - S${formatSeriesNumber(
-        data.season
-      )}E${formatSeriesNumber(data.number)}`
-      description.innerHTML = data.summary
-      globalContainer.append(dataEl)
-      dataEl.append(title)
-      title.append(link)
-      dataEl.append(image)
-      dataEl.append(description)
-    })
+    // console.log(data)
+    const globalContainer = document.querySelector('.grid-container')
+    const input = document.getElementById('search')
+    const numOfEpisodes = document.querySelector('#number_of_episodes')
+    input.addEventListener('input', () => search(input))
+    numOfEpisodes.innerHTML = `Displaying ${data.length}/${dataArr.length} episodes` // Display the number of episodes
+    dataArr.forEach((data) => renderElements(data, globalContainer))
   } catch (error) {
     console.log(error)
   }
 }
-function setup() {
-  const allEpisodes = getAllEpisodes()
-  makePageForEpisodes(allEpisodes)
-  let isLoading = false
-}
+
+// Function to format the series number
 function formatSeriesNumber(number) {
   return number < 10 ? '0' + number : number
 }
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById('root')
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`
+
+// Function to search for episodes
+function search({ value: inputValue }) {
+  const filter = inputValue.toUpperCase().trim()
+  const posts = Array.from(document.querySelectorAll('.post-container'))
+  let count = 0
+
+  posts.forEach((post) => {
+    const title = post.querySelector('h2')
+    const txtValue = title.textContent.trim()
+
+    if (txtValue.toUpperCase().includes(filter)) {
+      post.style.display = ''
+      count++
+    } else {
+      post.style.display = 'none'
+    }
+  })
+
+  const numOfEpisodes = document.querySelectorAll('#number_of_episodes')
+  numOfEpisodes.forEach((el) => {
+    el.innerHTML = `Displaying ${count}/${posts.length} episodes`
+  })
+}
+
+// Function to render the elements on the page
+function renderElements(element, parentElement) {
+  const dataEl = document.createElement('div')
+  const title = document.createElement('h2')
+  const link = document.createElement('a')
+  const image = document.createElement('img')
+  const description = document.createElement('p')
+  dataEl.setAttribute('id', element.id)
+  dataEl.setAttribute('class', 'post-container')
+  image.setAttribute('src', element.image.medium)
+  link.setAttribute('href', element.url)
+  link.innerHTML = `${element.name} - S${formatSeriesNumber(
+    element.season
+  )}E${formatSeriesNumber(element.number)}`
+  description.innerHTML = element.summary
+  parentElement.append(dataEl)
+  dataEl.append(title)
+  title.append(link)
+  dataEl.append(image)
+  dataEl.append(description)
 }
 
 window.onload = setup
-window.onload = getData
